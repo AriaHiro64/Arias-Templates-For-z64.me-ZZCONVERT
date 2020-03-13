9a7a14ac30ab1e087d04bcd833b71caa30eac463
ariahiro64 <ariahiro64@gmail.com>
2020-03-13 02:27:41 -0400

    ww aria and fd fix

--- Aria_Example/Display Lists/obj.h
+++ Aria_Example/Display Lists/obj.h
@@ -12,78 +12,44 @@
      #define   TEX_MOUTH_1     0x06004400
      #define   TEX_MOUTH_2     0x06004800
      #define   TEX_MOUTH_3     0x06004C00
-     #define   TEX_MOUTH_4     0x06005000
-     #define   TEX_MOUTH_5     0x06005400
 
 /* Display Lists * * * */
-     #define   DL_BOOMERANG     0x06011CA8
-     #define   DL_FPS_FOREARM_L     0x06012088
-     #define   DL_FPS_HAND_L     0x06012148
-     #define   DL_BOW_STRING     0x060123A0
-     #define   DL_FPS_HAND_R     0x06012C28
-     #define   DL_HILT_2     0x06013F98
-     #define   DL_BROKEN_BLADE_3     0x06014680
-     #define   DL_HILT_3     0x06014D80
-     #define   DL_FPS_FOREARM_R     0x060151D0
-     #define   DL_FPS_HAND_SHOT     0x06015CC0
-     #define   DL_OCARINA_1     0x06016150
-     #define   DL_FOOT_2_R_ZZLMTX     0x06016540
-     #define   DL_FOOT_3_R_ZZLMTX     0x06016B30
-     #define   DL_FOOT_2_L_ZZLMTX     0x06017080
-     #define   DL_FOOT_3_L_ZZLMTX     0x06017670
-     #define   DL_OCARINA_2     0x06017A70
-     #define   DL_SHOT_HAND_R     0x06017D90
-     #define   DL_SHEATH     0x06018180
-     #define   DL_CROSSHAIR     0x06018320
-     #define   DL_HILT_1     0x06018950
-     #define   DL_SHEATH_1     0x06018E90
-     #define   DL_SHIELD_1     0x06019258
-     #define   DL_HAMMER     0x06019B60
-     #define   DL_GAUNTLET_HAND_R_ZZLMTX     0x0601A018
-     #define   DL_GAUNTLET_HAND_L_ZZLMTX     0x0601A3B0
-     #define   DL_GAUNTLET_FOREARM_R_ZZLMTX     0x0601A728
-     #define   DL_GAUNTLET_FOREARM_L_ZZLMTX     0x0601AB08
-     #define   DL_GAUNTLET_FIST_R_ZZLMTX     0x0601AF08
-     #define   DL_GAUNTLET_FIST_L_ZZLMTX     0x0601B2A0
-     #define   DL_FIST_R     0x0601B5A8
-     #define   DL_FIST_L     0x0601B9E8
-     #define   DL_BOW     0x0601C6F0
-     #define   DL_BLADE_2     0x0601D270
-     #define   DL_BOTTLE_HAND_L     0x0601D740
-     #define   DL_BLADE_3     0x0601E330
-     #define   DL_OCARINA_HAND_R     0x0601E988
-     #define   DL_SHIELD_2     0x0601F2B8
+     #define   DL_FPS_FOREARM_R     0x0600A820
+     #define   DL_BLADE_3     0x0600AE90
+     #define   DL_HILT_3     0x0600B9E0
+     #define   DL_SWORD_4     0x0600C720
+     #define   DL_BOTTLE_HAND_L     0x0600CE08
+     #define   DL_FPS_HAND_L     0x0600DE08
+     #define   DL_OCARINA_2     0x0600E2A0
+     #define   DL_SHEATH_3     0x0600E6D0
+     #define   DL_FIST_R     0x0600E9E8
+     #define   DL_FIST_L     0x0600EE20
+     #define   DL_HILT_1     0x0600F658
+     #define   DL_SHIELD_1     0x0600FFB8
 
 /* Collisions * * * */
 
 /* Hierarchical Display Lists * * * */
-     #define   LIMB_LIMB_1     0x06020208
-     #define   LIMB_LIMB_3     0x060208E8
-     #define   LIMB_LIMB_4     0x06020DD0
-     #define   LIMB_LIMB_5     0x06021350
-     #define   LIMB_LIMB_6     0x060216C8
-     #define   LIMB_LIMB_7     0x06021A30
-     #define   LIMB_LIMB_8     0x06021EA0
-     #define   LIMB_LIMB_10     0x06023318
-     #define   LIMB_LIMB_11     0x06023FF0
-     #define   LIMB_LIMB_12     0x06024B30
-     #define   LIMB_LIMB_13     0x06024E88
-     #define   LIMB_LIMB_14     0x06025318
-     #define   LIMB_LIMB_15     0x06025798
-     #define   LIMB_LIMB_16     0x06025A68
-     #define   LIMB_LIMB_17     0x06025EF8
-     #define   LIMB_LIMB_18     0x060263D8
-     #define   LIMB_LIMB_19     0x06026608
-     #define   LIMB_LIMB_20     0x060273B8
-
-/* Animations * * * */
-     #define   ANIM_IDLE_3240     0x06027B08
-     #define   ANIM_LONG_STAND_2BE0     0x06027C84
-     #define   ANIM_OCARINA_UP_30A0     0x06027EE8
-     #define   ANIM_OCARINA_30A8     0x06028044
-     #define   ANIM_POSE     0x06028110
+     #define   LIMB_LIMB_1     0x06010EF0
+     #define   LIMB_LIMB_3     0x060114E0
+     #define   LIMB_LIMB_4     0x060118E8
+     #define   LIMB_LIMB_5     0x06011E60
+     #define   LIMB_LIMB_6     0x060121D0
+     #define   LIMB_LIMB_7     0x06012538
+     #define   LIMB_LIMB_8     0x060129A0
+     #define   LIMB_LIMB_10     0x06013E08
+     #define   LIMB_LIMB_11     0x06014A70
+     #define   LIMB_LIMB_12     0x060155B0
+     #define   LIMB_LIMB_13     0x06015908
+     #define   LIMB_LIMB_14     0x06015D88
+     #define   LIMB_LIMB_15     0x06016208
+     #define   LIMB_LIMB_16     0x060164D8
+     #define   LIMB_LIMB_17     0x06016958
+     #define   LIMB_LIMB_18     0x06016E38
+     #define   LIMB_LIMB_19     0x06017068
+     #define   LIMB_LIMB_20     0x06017E18
 
 /* Hierarchies (Skeletons) * * * */
-     #define   SKL_DEFAULT     0x060282C4
+     #define   SKL_DEFAULT     0x060183F0
 
 /* Base Offset: 0x06000000 */
